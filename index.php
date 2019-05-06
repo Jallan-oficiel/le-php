9b109e69b86eb2f84c01fbb83b54ffa432d39bb5
Jallan <alexandre.jallan@viacesi.fr>
2019-05-06 14:11:59 +0200

    Définition d'une constante

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
-$version = "7";
-$message = "Je fais du php";
+$version = 7;
+$message = '<h1>Je fais du php ' .$version . '</h1>';
 echo $message;
+
+// Utilisation des constantes
+define('TVATxPlein', 0.2);
